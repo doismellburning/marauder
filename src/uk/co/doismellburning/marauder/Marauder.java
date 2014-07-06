4d1b37e2516a04c30d683370272004b6ca72c788
Kristian Glass <git@doismellburning.co.uk>
2014-07-06 21:04:52 +0100

    Write data to JSON
    (Probably)

--- src/uk/co/doismellburning/marauder/Marauder.java
+++ src/uk/co/doismellburning/marauder/Marauder.java
@@ -8,11 +8,20 @@ import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.JsonWriter;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.AbstractList;
+import java.util.Date;
 import java.util.List;
 
 public class Marauder extends Activity {
@@ -47,7 +56,7 @@ public class Marauder extends Activity {
 
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		wifiManager.startScan();
-		mainText.setText("Starting Scan");
+		mainText.append("\n\nRefreshing...");
 		return super.onMenuItemSelected(featureId, item);
 	}
 
@@ -67,12 +76,61 @@ public class Marauder extends Activity {
 		public void onReceive(Context context, Intent intent) {
 			StringBuilder sb = new StringBuilder();
 			List<ScanResult> wifiList = wifiManager.getScanResults();
+			JSONArray data = new JSONArray();
 			for(int i = 0; i < wifiList.size(); i++){
 				sb.append(new Integer(i+1).toString() + ".");
-				sb.append((wifiList.get(i)).toString());
-				sb.append("\\n");
+				ScanResult scanResult = wifiList.get(i);
+				sb.append(scanResult.SSID + ": " + scanResult.level);
+				sb.append("\n");
+				data.put(scanResultToJSON(scanResult));
 			}
 			mainText.setText(sb);
+			writeData(data);
 		}
+	}
+
+	private void writeData(JSONArray data) {
+		File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+		Date d = new Date();
+		DateFormat df = new SimpleDateFormat("yyyy/MM/dd'T'HH:mm:ss");
+		String name = df.format(d);
+		File output = new File(path, name + ".json");
+
+		try {
+			output.mkdirs();
+
+			OutputStream os = new FileOutputStream(output);
+			Writer w = new OutputStreamWriter(os);
+			w.write(data.toString());
+			w.close();
+			os.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static boolean isExternalStorageWritable() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static JSONObject scanResultToJSON(ScanResult scanResult) {
+		JSONObject o = new JSONObject();
+		try {
+			o.put("BSSID", scanResult.BSSID);
+			o.put("capabilities", scanResult.capabilities);
+			o.put("frequency", scanResult.frequency);
+			o.put("level", scanResult.level);
+			o.put("SSID", scanResult.SSID);
+			// o.put("timestamp", scanResult.timestamp); // TODO Requires SDK > myphone
+		} catch (JSONException e) {
+			e.printStackTrace(); // Sigh
+		}
+		return o;
 	}
 }
